cf45f87c7e0bc489f72f5fda34b650b4ad94c92d
wguner <setenay.guner@wsu.edu>
2021-04-10 18:16:19 -0700

    finished node and edge func

--- bdd.py
+++ bdd.py
@@ -1,6 +1,6 @@
-#from pyeda.inter import *
+from pyeda.inter import *
 
-def create_var(num):
+def create_node(num):
 	# Converts a number into binary then to an array of 5 bits
     binary = ('{0:05b}'.format(num))
     str2arr = []
@@ -8,8 +8,12 @@ def create_var(num):
         str2arr.append(elem)
     return [int(intList) for intList in str2arr]
 
-def create_bool_formula(i, j):
-	# takes to numbers converts to binary array then appends it to a new list
-	ex1 = var_expression('x', create_var(i))
-	ex2 = var_expression('y', create_var(j))
-	return ex1 & ex2
+def create_edge(i, j):
+	# takes two numbers converts to binary array then appends it to a new list
+    # [0, 1, 1, 0, 0, 0, 1, 0, 1, 1]
+    i = create_node(i)
+    j = create_node(j)
+    formula = []
+    formula.append(i)
+    formula.append(j)
+    return [item for elem in formula for item in elem]
